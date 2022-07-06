98b2798de6fc88d8831a8fb6fd24a0fcc3444b82
lijiajun <2281931113@qq.com>
2022-07-06 11:53:56 +0800

    Initial commit

---
+++ src/main/java/com/ljj/domain/Department.java
@@ -0,0 +1,10 @@
+package com.ljj.domain;
+
+import lombok.Data;
+
+@Data
+public class Department {
+    private Integer deptId;
+
+    private String deptName;
+}
---
+++ src/main/java/com/ljj/service/IBookCategoryService.java
@@ -0,0 +1,12 @@
+package com.ljj.service;
+
+import com.ljj.domain.BookCategory;
+import com.ljj.utils.page.Page;
+
+public interface IBookCategoryService {
+
+    //图书类别分页查询
+    public Page<BookCategory> selectBookCategoryByPageNum(int pageNum);
+
+    int deleteBookCategoryById(int bookCategoryId);
+}
